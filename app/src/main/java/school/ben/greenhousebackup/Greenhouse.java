474ec7637def4792a918afda6bf9bb868b398dfa
Sarah Colborne <skcolborne@gmail.com>
2017-06-02 11:23:57 -0300

    quick commit before testing new branch

--- app/src/main/java/school/ben/greenhousebackup/Greenhouse.java
+++ app/src/main/java/school/ben/greenhousebackup/Greenhouse.java
@@ -5,4 +5,29 @@ package school.ben.greenhousebackup;
  */
 
 public class Greenhouse {
+    Temperature temperature;
+    Humidity humidity;
+    LightLevel lightLevel;
+
+    public void checkTempInRange(){
+        if (!temperature.checkCurrTempInRange()) {
+            //handle bad temp
+        }
+
+    }
+
+    public void checkHumidityInRange(){
+        if (!humidity.checkCurrHumidityInRange()) {
+            //handle bad humidity
+        }
+
+    }
+
+    public void checkLightInRange(){
+        if (!lightLevel.checkCurrLightInRange()) {
+            //handle bad light level
+        }
+
+    }
+
 }
