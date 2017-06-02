5151ce22d717d0591f918fb5c219163897650161
seanmahoney <seunmahoney@gmail.com>
2017-06-02 09:28:57 -0300

    hexiwear and firebase

--- app/src/main/java/school/ben/greenhousebackup/MainActivity.java
+++ app/src/main/java/school/ben/greenhousebackup/MainActivity.java
@@ -3,6 +3,8 @@ package school.ben.greenhousebackup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.wolkabout.hexiwear.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
